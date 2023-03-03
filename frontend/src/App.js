78b49804d1ac81f3fa8139c07bd25ffaa7c8c759
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2023-03-03 15:35:36 +0700

    2. refactor UI signUpand login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,28 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import Login from "./components/Login";
-import Messenger from "./components/Messenger";
-import ProtectRoute from "./components/ProtectRoute";
-import Register from "./components/Register";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Messenger from './components/Messenger';
+import ProtectRoute from './components/ProtectRoute';
+import Register from './components/Register';
 
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-    <Routes>
-      <Route path="/messenger/login" element={<Login />} />
-      <Route path="/messenger/register" element={<Register />} /> 
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
 
-      <Route path="/" element={ <ProtectRoute> <Messenger /> </ProtectRoute> } />
-
-      
-    </Routes>
-  </BrowserRouter>,
-      
-    </div>
-  );
+                <Route
+                    path="/"
+                    element={
+                        <ProtectRoute>
+                            {' '}
+                            <Messenger />{' '}
+                        </ProtectRoute>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
